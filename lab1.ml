dd28ca900b868b42a90d0d553c5831c884d7a3e7
ColtonJones197 <ckjones97@ksu.edu>
2023-08-28 01:00:56 -0500

    added method sigs

--- lab1.ml
+++ lab1.ml
@@ -9,12 +9,12 @@ let rec repeat f (x: int) (n: int) =
   | _ -> f (repeat f x (n - 1));;
 (* repeat (fun x -> x + 7) 4 3;; *)
 
-let rec times (x: int) (n: int) =
+let times (x: int) (n: int) =
   repeat ((+) x) x (n-1);;
 
 (* times 11 7;; *)
 
-let rec power base pow =
+let power base pow =
   repeat (times base) base (pow - 1);;
 
 (* power 5 4;; *)
